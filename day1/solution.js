4ccf62990fa03c23373b7075544323b54b1feaf4
Kelvin Moses <moseskelvin4u@gmail.com>
2022-12-26 14:50:47 +0100

    containsDuplicate

---
+++ day1/solution.js
@@ -0,0 +1,10 @@
+var containsDuplicate = function (nums) {
+  let frequency = {}
+  for (let val of nums) {
+    frequency[val] = (frequency[val] || 0) + 1
+    if (frequency[val] > 1) {
+      return true
+    }
+  }
+  return false
+}
