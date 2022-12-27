3cfbf2dd709c2cd585117288932b45e19c835e46
Maureen <amaka013@gmail.com>
2022-12-27 23:06:22 +0100

    Added solutions day 1 and 2
    Signed-off-by: Maureen <amaka013@gmail.com>

---
+++ Valid_Anagram/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+            if len(s) != len(t):
+                return False
+            s_dict={}
+            t_dict={}
+            for letter in s:
+                s_dict[letter]=1+s_dict.get(letter, 0)
+            
+            for letter in t:
+                t_dict[letter]=1+t_dict.get(letter, 0)
+            
+            for letter in s_dict:
+                if letter not in t_dict or s_dict[letter] != t_dict[letter]:
+                    return False
+                
+            # for letter in t_dict:
+            #     if letter not in s_dict  or s_dict[letter] != t_dict[letter]:
+            #         return False
+            return True
+                
