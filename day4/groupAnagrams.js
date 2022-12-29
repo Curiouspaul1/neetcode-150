95791c51139012e8d2de2b46d49ae771ebf37ed7
Toyosi Adekanmbi <oluwatoyosiadeleye4@gmail.com>
2022-12-29 21:01:10 +0100

    day4

---
+++ day4/groupAnagrams.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */    
+var groupAnagrams = function(strs){
+	let lookUp = {};
+    let result = [];
+    for (let i = 0; i < strs.length; i++) {
+        let key = strs[i].split("").sort();
+        if (!lookUp[key]) lookUp[key] = [strs[i]];
+        else lookUp[key].push(strs[i]); 
+    }
+    for (let key of lookUp) {
+        result.push(lookUp[key]);
+    }
+    return result;
+}
