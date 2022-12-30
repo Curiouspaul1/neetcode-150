9e334b31597ad7e3a8ae87b950e2e6058182de58
Kelvin Moses <moseskelvin4u@gmail.com>
2022-12-30 14:28:38 +0100

    day 4

---
+++ day4/solution.js
@@ -0,0 +1,14 @@
+var groupAnagrams = function (strs) {
+  let result = {}
+  for (let word of strs) {
+    let sortedWord = word.split('').sort().join('')
+    if (result[sortedWord]) {
+      result[sortedWord].push(word)
+    } else {
+      result[sortedWord] = [word]
+    }
+  }
+  return Object.values(result)
+}
+
+groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])
