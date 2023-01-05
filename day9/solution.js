82ea46bec1a60cdafce0884ec1bf017730ff846b
Kelvin Moses <moseskelvin4u@gmail.com>
2023-01-05 10:04:07 +0100

    day9 and day 10 challenge

--- day9/solution.js
+++ day9/solution.js
@@ -1,25 +1,11 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {string} s
+ * @return {boolean}
  */
-function longestConsecutive(nums) {
-  if (nums == null || nums.length === 0) return 0
-
-  const set = new Set(nums)
-  let max = 0
-
-  for (let num of set) {
-    if (set.has(num - 1)) continue // make sure starting from the beginning of sequence
-
-    let currNum = num
-    let currMax = 1
-
-    while (set.has(currNum + 1)) {
-      currNum++
-      currMax++
-    }
-    max = Math.max(max, currMax)
+var isPalindrome = function (s) {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '')
+  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+    if (s.charAt(i) !== s.charAt(j)) return false
   }
-
-  return max
+  return true
 }
