00de7b0894bc63cbb00cf04d1b4974c7158b052c
Toyosi Adekanmbi <oluwatoyosiadeleye4@gmail.com>
2023-01-07 20:51:04 +0100

    day12

--- day12/maxArea.js
+++ day12/maxArea.js
@@ -4,17 +4,14 @@
  */
 var maxArea = function(height) {
   let l = 0, r = height.length - 1;
-  let area = 0;
-  let lookUp = [];
-  while (l < r) {
+  let area = Math.min(height[l], height[r]) * (r - l);
+  let maxArea = area;
+  while (r > l) {
     area = Math.min(height[l], height[r]) * (r - l);
     if (height[l] < height[r]) l++;
     else if (height[l] > height[r]) r--;
-    else {
-        l++,
-        r--;
-    };
-    lookUp.push(area);
+    else { l++; r-- }
+    if (area > maxArea) maxArea = area;
   }
-  return lookUp.reduce((a, b) => Math.max(a, b), -Infinity);
+  return maxArea;
 };
