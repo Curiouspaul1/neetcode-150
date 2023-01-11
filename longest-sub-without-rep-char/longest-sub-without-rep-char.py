d7370b5ea4c6d9f77abded23b25f6be26362da6c
Richie <molunorichie@gmail.com>
2023-01-12 00:46:15 +0100

    Day 15 - Longest substr without repeating characters

---
+++ longest-sub-without-rep-char/longest-sub-without-rep-char.py
@@ -0,0 +1,14 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        store = {}
+        start = 0
+        length = 0
+
+        for i in range(len(s)):
+            if s[i] in store and start <= store[s[i]]:
+                start = store[s[i]] + 1
+
+            length = max(length, (i - start) + 1)
+            store[s[i]] = i
+
+        return length
