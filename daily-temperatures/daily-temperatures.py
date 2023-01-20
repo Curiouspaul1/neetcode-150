212392ca1573ab027b95979adc524a24fb4b447d
Richie <molunorichie@gmail.com>
2023-01-21 00:10:52 +0100

    Day 24 - Daily tempertures

---
+++ daily-temperatures/daily-temperatures.py
@@ -0,0 +1,12 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        stack = [] #[index, temp]
+        
+        for i, t in enumerate(temperatures):
+#is stack empty and is temperature greater than temperature on top of stack
+            while stack and t > stack[-1][0]:
+                stackT, stackI = stack.pop()
+                res[stackI] = (i - stackI)
+            stack.append([t, i])
+        return res
