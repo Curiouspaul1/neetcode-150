08d970c4305029c6513fcf13b14129899ab3c952
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-03-08 07:04:07 +0100

    Day 4 completed in grand style

---
+++ Day64/d64.py
@@ -0,0 +1,11 @@
+from ast import List
+
+
+def lastStoneWeight( stones: List[int]) -> int:
+    while len(stones) > 1:
+        stones.sort()
+        y = stones.pop()
+        x = stones.pop()
+        if x != y:
+            stones.append(y - x)
+    return stones[0] if stones else 0
