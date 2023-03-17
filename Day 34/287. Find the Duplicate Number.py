4e4a6b06d409ee7cf099e0c938431eb0c3e29aca
Oyaleke Philip <Oyaleke.philip@gmail.com>
2023-03-17 20:40:48 +0100

    Day 34

---
+++ Day 34/287. Find the Duplicate Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findDuplicate(self, nums) -> int:
+ 
+   
+        dic={}
+
+        for i in nums:
+            if i in dic:
+                dic[i]=dic[i]+1
+            else:
+                dic[i]=1
+        
+        for i in dic:
+            if dic[i]>=2:
+                return i
+        return 0
+        
