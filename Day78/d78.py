dd5c5d354258ed9823d0e95421704be9e7bcd632
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-03-24 12:39:01 +0100

    Day 78 done with dubs

---
+++ Day78/d78.py
@@ -0,0 +1,49 @@
+# Copilot Solution
+def solveNQueens(n):
+    def DFS(queens, xy_dif, xy_sum):
+        p = len(queens)
+        if p==n:
+            result.append(queens)
+            return None
+        for q in range(n):
+            if q not in queens and p-q not in xy_dif and p+q not in xy_sum: 
+                DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])  
+    result = []
+    DFS([],[],[])
+    return [ ["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+
+
+# Neetcode solution
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set()  # (r + c)
+        negDiag = set()  # (r - c)
+
+        res = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
