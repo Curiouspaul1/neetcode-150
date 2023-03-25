0e91b47592c4db1d9432c097fcb7d0b53340f786
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-03-25 12:10:06 +0100

    Day 79 completed with dubs

--- Day78/d78.py
+++ Day78/d78.py
@@ -1,16 +1,19 @@
 # Copilot Solution
+from typing import List
+
+
 def solveNQueens(n):
     def DFS(queens, xy_dif, xy_sum):
         p = len(queens)
-        if p==n:
+        if p == n:
             result.append(queens)
             return None
         for q in range(n):
-            if q not in queens and p-q not in xy_dif and p+q not in xy_sum: 
-                DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])  
+            if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
+                DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])
     result = []
-    DFS([],[],[])
-    return [ ["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+    DFS([], [], [])
+    return [["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
 
 
 # Neetcode solution
@@ -47,3 +50,19 @@ class Solution:
 
         backtrack(0)
         return res
+
+
+# creating a multiplication table
+def nMultiplicationTable(n):
+    res = []
+    curRow = [i for i in range(1, n+1)]
+    for n in range(1, n+1):
+        curRes = [n * i for i in curRow]
+        res.append(curRes)
+        curRes = []
+    return res
+
+assert nMultiplicationTable(5)== [[1, 2, 3, 4, 5], [2, 4, 6, 8, 10], [3, 6, 9, 12, 14], [4, 8, 12, 16, 20], [5, 10, 15, 20, 25]]
+
+
+#print(nMultiplicationTable(5))
